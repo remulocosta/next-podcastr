14a112731d4d54e029a32c2883d4edcf0a6993a7
Remulo Costa <remulo.costa@vulcanet.com.br>
2021-04-24 15:30:00 -0300

    Implement context api PlayerContext in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,49 @@
 import '../styles/global.scss'
 
+import { useState } from 'react';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 
 import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpissodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpissodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setIsPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{
+      episodeList,
+      currentEpisodeIndex,
+      isPlaying,
+      play,
+      setIsPlayingState,
+      togglePlay
+    }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
